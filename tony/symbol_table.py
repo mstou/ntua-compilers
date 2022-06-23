78b9b70d6dfa245b65061e406779f86186c30184
Miltiadis Stouras <m.stouras@gmail.com>
2022-06-23 12:35:15 +0300

    Fix minor bugs in the symbol table
    Co-authored-by: Iasonas Nikolaou <iasonasnikolaou11@hotmail.com>

--- tony/symbol_table.py
+++ tony/symbol_table.py
@@ -1,17 +1,17 @@
 from enum import Enum
 
 class Type(Enum):
-   INT = 1
+   INT  = 1
    CHAR = 2
    BOOL = 3
    VOID = 4
-   NIL = 5
+   NIL  = 5
 
 class SymbolEntry:
     def __init__(self, type):
         self.type = type
-    def __repr__(self):
-        return self.type
+    def __str__(self):
+        return str(self.type)
 
 class Scope:
     def __init__(self):
@@ -19,7 +19,7 @@ class Scope:
 
     def lookup(self, s):
         if s in self.locals.keys():
-            return locals[s]
+            return self.locals[s]
         return None
 
     def insert(self, s, t): #insert name s with type t.
@@ -27,9 +27,10 @@ class Scope:
             print("Error. Name already in scope...")
             return
         self.locals[s] = SymbolEntry(t)
-    def __repr__(self):
-        return "\n".join([k + " " + str(v) for k, v in self.locals.items()])
-    
+
+    def __str__(self):
+        return "\n".join([f'{k} -> {v}' for k, v in self.locals.items()])
+
 class SymbolTable:
     def __init__(self):
         self.scopes = []
@@ -43,37 +44,41 @@ class SymbolTable:
     def insert(self, s, t):
         if len(self.scopes) == 0:
             self.scopes.append(Scope())
-        sc = self.scopes[len(self.scopes)-1]
+        sc = self.scopes[-1]
         sc.insert(s, t)
 
     def lookup(self, s):
-        for sc in self.scopes.reverse():
+        for sc in self.scopes[::-1]: # reverse
             name = sc.lookup(s)
             if name != None:
                 return name
+
         return None
 
-    def __repr__(self):
-        s = ""
+    def __str__(self):
+        s = ''
         for index, sc in enumerate(self.scopes):
-            s += "-Scope " + str(index) + "-\n" + str(sc) + "\n"
-        return s
-            
+            s += f'\n-- Scope {index} --\n'
+            s += f'{sc}\n'
 
-st = SymbolTable()
-st.openScope()
-st.insert("x", "Type.INT")
-print(st)
-st.insert("y", "Type.BOOL")
-print(st.lookup("x"))
-print(st)
-st.openScope()
-st.insert("x", "Type.BOOL")
-print(st.lookup("x"))
-print(st.lookup("y"))
-print(st)
-st.closeScope()
-print(st)
-print(st.lookup("x"))
-st.closeScope()
-print(st)
+        s += '----------------------\n'
+        return s
+
+
+# st = SymbolTable()
+# st.openScope()
+# st.insert("x", Type.INT)
+# print(st)
+# st.insert("y", Type.BOOL)
+# print(f'x look up: {st.lookup("x")}')
+# print(st)
+# st.openScope()
+# st.insert("x", Type.BOOL)
+# print(st)
+# print(f'x look up: {st.lookup("x")}')
+# print(f'y look up: {st.lookup("y")}')
+# st.closeScope()
+# print(st)
+# print(f'x look up: {st.lookup("x")}')
+# st.closeScope()
+# print(st)

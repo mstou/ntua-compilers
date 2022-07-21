3a1d9973bcb17f8a5c234a008a07d6d7068f2876
Miltiadis Stouras <m.stouras@gmail.com>
2022-07-21 13:40:50 +0300

    Move the compilation pipeline inside a function. Use rpath instead of export for gcc

--- tonyc.py
+++ tonyc.py
@@ -14,22 +14,42 @@ def readFile(file):
         s = f.read()
     return s
 
-argparser = argparse.ArgumentParser()
-argparser.add_argument('filename', default='', type=str)
-args = argparser.parse_args()
+def compile(file,
+            llvm_output=LLVM_OUTPUT,
+            as_output=AS_OUTPUT,
+            executable=EXECUTABLE,
+            show_commands=False):
 
-if BUILTINS_LIB not in os.listdir():
-    os.system('make builtins')
+    if BUILTINS_LIB not in os.listdir():
+        cmd_make_builtins = '$(make) builtins'
+        os.system(cmd_make_builtins)
 
-file = args.filename
-code = readFile(file)
-ast = parser.parse(code)
-ast.sem(SymbolTable())
-with open(LLVM_OUTPUT, 'w') as f:
-    print(ast.codegen(), file = f)
+        if show_commands: print(cmd_make_builtins)
 
-os.system(f'llc {LLVM_OUTPUT} --relocation-model=pic -o {AS_OUTPUT}')
-os.system(f'gcc {AS_OUTPUT} -L . -lbuiltins -o {EXECUTABLE}')
-os.system(f'export LD_LIBRARY_PATH={os.getcwd()}')
-os.remove(LLVM_OUTPUT)
-os.remove(AS_OUTPUT)
+    code = readFile(file)
+    ast = parser.parse(code)
+    ast.sem(SymbolTable())
+    with open(LLVM_OUTPUT, 'w') as f:
+        print(ast.codegen(), file = f)
+
+    cmd_llc = f'llc {llvm_output} --relocation-model=pic -o {as_output}'
+    os.system(cmd_llc)
+    if show_commands: print(cmd_llc)
+
+    cmd_gcc = f'gcc {as_output} -L . -Wl,-rpath={os.getcwd()} -lbuiltins -o {executable}'
+    os.system(cmd_gcc)
+    if show_commands: print(cmd_gcc)
+
+    os.remove(llvm_output)
+    if show_commands: print(f'rm {llvm_output}')
+
+    os.remove(as_output)
+    if show_commands: print(f'rm {as_output}')
+
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('filename', default='', type=str)
+    args = argparser.parse_args()
+
+    compile(args.filename, show_commands=False)

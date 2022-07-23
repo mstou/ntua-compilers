cadf74f615b6daf2279c50f7cc23955e05c6eafa
Miltiadis Stouras <m.stouras@gmail.com>
2022-07-23 21:09:44 +0300

    Add string reversing to end2end tests

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -35,3 +35,18 @@ def test_primes():
         assert result.stdout.decode("utf-8") == expected_output
 
     os.remove('a.out')
+
+@pytest.mark.end2end
+def test_string_reverse():
+    compile(CORRECT_PROGRAMS + 'string_reverse.tony')
+
+    result = subprocess.run('./a.out', shell=True, stdout=subprocess.PIPE)
+    output_file = TEST_INPUTS + 'string_reverse/output_1.txt'
+    expected_output = ''
+
+    with open(output_file, 'r') as f:
+        expected_output = f.read()
+
+    assert result.stdout.decode("utf-8") == expected_output
+
+    os.remove('a.out')

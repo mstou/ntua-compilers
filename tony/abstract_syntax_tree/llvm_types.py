d56617e6e00b931d08ee91212e5e7680ea16b0ab
Miltiadis Stouras <m.stouras@gmail.com>
2022-07-24 14:01:34 +0300

    Implement list nodes in llvm

--- tony/abstract_syntax_tree/llvm_types.py
+++ tony/abstract_syntax_tree/llvm_types.py
@@ -1,5 +1,18 @@
 from llvmlite import ir
-from .data_types   import BaseType, Array
+from .data_types   import BaseType, Array, List
+
+class LLVM_Sizes():
+    ''' Bit size of types '''
+    Int  = 32 # 4 bytes
+    Char = 8  # 1 byte
+    Bool = 1  # 1 bit
+
+class LLVM_Types():
+    Int  = ir.IntType(LLVM_Sizes.Int)
+    Char = ir.IntType(LLVM_Sizes.Char)
+    Bool = ir.IntType(LLVM_Sizes.Bool)
+    Void = ir.VoidType()
+
 
 def BaseType_to_LLVM(type):
     if type == BaseType.Int:
@@ -14,16 +27,36 @@ def BaseType_to_LLVM(type):
     if isinstance(type, Array):
         return BaseType_to_LLVM(type.t).as_pointer()
 
+    if isinstance(type, List):
+        # basic types
+        if type.t == BaseType.Int:
+            return IntList
+        if type.t == BaseType.Bool:
+            return BoolList
+        if type.t == BaseType.Char:
+            return CharList
+
+        else:
+            return LLVM_List(type.t)
+
     return None
 
-class LLVM_Sizes():
-    ''' Bit size of types '''
-    Int  = 32 # 4 bytes
-    Char = 8  # 1 byte
-    Bool = 1  # 1 bit
+static_id = 0
 
-class LLVM_Types():
-    Int  = ir.IntType(LLVM_Sizes.Int)
-    Char = ir.IntType(LLVM_Sizes.Char)
-    Bool = ir.IntType(LLVM_Sizes.Bool)
-    Void = ir.VoidType()
+def LLVM_List(type):
+    global static_id
+
+    if isinstance(type, List):
+        return LLVM_List(LLVM_List(type.t))
+
+    llvm_type = BaseType_to_LLVM(type)
+    ctx  = ir.global_context
+    node = ctx.get_identified_type(f'list.{type}_{static_id}')
+    static_id += 1
+    node.set_body(llvm_type, node.as_pointer())
+
+    return node
+
+IntList  = LLVM_List(BaseType.Int)
+BoolList = LLVM_List(BaseType.Bool)
+CharList = LLVM_List(BaseType.Char)

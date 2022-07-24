8d2b35285585acc25b97f7ff3290786d2468ea21
Miltiadis Stouras <m.stouras@gmail.com>
2022-07-24 14:42:00 +0300

    Keep a dictionary for all the list types instantiated to avoid defining duplicates in the code

--- tony/abstract_syntax_tree/llvm_types.py
+++ tony/abstract_syntax_tree/llvm_types.py
@@ -28,35 +28,25 @@ def BaseType_to_LLVM(type):
         return BaseType_to_LLVM(type.t).as_pointer()
 
     if isinstance(type, List):
-        # basic types
-        if type.t == BaseType.Int:
-            return IntList
-        if type.t == BaseType.Bool:
-            return BoolList
-        if type.t == BaseType.Char:
-            return CharList
-
-        else:
-            return LLVM_List(type.t)
+        return LLVM_List(type.t)
 
     return None
 
-static_id = 0
+already_instantiated = {}
 
 def LLVM_List(type):
-    global static_id
+    global already_instantiated
+
+    if str(type) in already_instantiated:
+        return already_instantiated[str(type)]
 
     if isinstance(type, List):
         return LLVM_List(LLVM_List(type.t))
 
     llvm_type = BaseType_to_LLVM(type)
     ctx  = ir.global_context
-    node = ctx.get_identified_type(f'list.{type}_{static_id}')
-    static_id += 1
+    node = ctx.get_identified_type(f'list.{type}')
     node.set_body(llvm_type, node.as_pointer())
+    already_instantiated[str(type)] = node
 
     return node
-
-IntList  = LLVM_List(BaseType.Int)
-BoolList = LLVM_List(BaseType.Bool)
-CharList = LLVM_List(BaseType.Char)

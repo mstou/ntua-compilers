10c16ab6dbaf4e6daeb020fd2d18ae914e52d7e4
Miltiadis Stouras <m.stouras@gmail.com>
2022-07-26 19:50:41 +0300

    Add more end to end tests

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -24,7 +24,7 @@ def test_array_addition():
 def test_array_addition_2d():
     compile(CORRECT_PROGRAMS + 'array_addition_2d.tony')
 
-    for i in range(1,3):
+    for i in range(1,4):
         output_file = TEST_INPUTS + f'array_addition_2d/output_{i}.txt'
         input_file  = TEST_INPUTS + f'array_addition_2d/input_{i}.txt'
 
@@ -34,6 +34,84 @@ def test_array_addition_2d():
             expected_output = f.read()
 
         assert result.stdout.decode("utf-8") == expected_output
+
+    os.remove('a.out')
+
+@pytest.mark.end2end
+def test_array_io():
+    compile(CORRECT_PROGRAMS + 'array_io.tony')
+
+    for i in range(1,3):
+        output_file = TEST_INPUTS + f'array_io/output_{i}.txt'
+        input_file  = TEST_INPUTS + f'array_io/input_{i}.txt'
+
+        result = subprocess.run(f'./a.out < {input_file}', shell=True, stdout=subprocess.PIPE)
+
+        with open(output_file, 'r') as f:
+            expected_output = f.read()
+
+        assert result.stdout.decode("utf-8") == expected_output
+
+    os.remove('a.out')
+
+@pytest.mark.end2end
+def test_bubblesort():
+    compile(CORRECT_PROGRAMS + 'bubblesort.tony')
+
+    result = subprocess.run('./a.out', shell=True, stdout=subprocess.PIPE)
+    output_file = TEST_INPUTS + 'bubblesort/output_1.txt'
+    expected_output = ''
+
+    with open(output_file, 'r') as f:
+        expected_output = f.read()
+
+    assert result.stdout.decode("utf-8") == expected_output
+
+    os.remove('a.out')
+
+@pytest.mark.end2end
+def test_function_call():
+    compile(CORRECT_PROGRAMS + 'function_call.tony')
+
+    result = subprocess.run('./a.out', shell=True, stdout=subprocess.PIPE)
+    output_file = TEST_INPUTS + 'function_call/output_1.txt'
+    expected_output = ''
+
+    with open(output_file, 'r') as f:
+        expected_output = f.read()
+
+    assert result.stdout.decode("utf-8") == expected_output
+
+    os.remove('a.out')
+
+@pytest.mark.end2end
+def test_function_mutual_recursion():
+    compile(CORRECT_PROGRAMS + 'function_mutual_recursion.tony')
+
+    result = subprocess.run('./a.out', shell=True, stdout=subprocess.PIPE)
+    output_file = TEST_INPUTS + 'function_mutual_recursion/output_1.txt'
+    expected_output = ''
+
+    with open(output_file, 'r') as f:
+        expected_output = f.read()
+
+    assert result.stdout.decode("utf-8") == expected_output
+
+    os.remove('a.out')
+
+
+@pytest.mark.end2end
+def test_function_pass_by_ref():
+    compile(CORRECT_PROGRAMS + 'function_pass_by_ref.tony')
+
+    result = subprocess.run('./a.out', shell=True, stdout=subprocess.PIPE)
+    output_file = TEST_INPUTS + 'function_pass_by_ref/output_1.txt'
+    expected_output = ''
+
+    with open(output_file, 'r') as f:
+        expected_output = f.read()
+
+    assert result.stdout.decode("utf-8") == expected_output
 
     os.remove('a.out')
 
@@ -81,6 +159,36 @@ def test_primes():
             expected_output = f.read()
 
         assert result.stdout.decode("utf-8") == expected_output
+
+    os.remove('a.out')
+
+@pytest.mark.end2end
+def test_quicksort():
+    compile(CORRECT_PROGRAMS + 'quicksort.tony')
+
+    result = subprocess.run('./a.out', shell=True, stdout=subprocess.PIPE)
+    output_file = TEST_INPUTS + 'quicksort/output_1.txt'
+    expected_output = ''
+
+    with open(output_file, 'r') as f:
+        expected_output = f.read()
+
+    assert result.stdout.decode("utf-8") == expected_output
+
+    os.remove('a.out')
+
+@pytest.mark.end2end
+def test_scopes():
+    compile(CORRECT_PROGRAMS + 'scopes.tony')
+
+    result = subprocess.run('./a.out', shell=True, stdout=subprocess.PIPE)
+    output_file = TEST_INPUTS + 'scopes/output_1.txt'
+    expected_output = ''
+
+    with open(output_file, 'r') as f:
+        expected_output = f.read()
+
+    assert result.stdout.decode("utf-8") == expected_output
 
     os.remove('a.out')
 

8cb17dc37f39e2fb09e837c9e46ac29d9c642eb5
Miltiadis Stouras <m.stouras@gmail.com>
2022-07-28 14:29:24 +0300

    Add -f and -i arguments to tonyc

--- tonyc.py
+++ tonyc.py
@@ -15,7 +15,7 @@ def readFile(file):
 def compile(file,
             llvm_to_stdout=False,
             as_to_stdout=False,
-            only_ast=False,
+            print_ast=False,
             show_commands=True,
             optimization=1,
             testing=False):
@@ -44,10 +44,11 @@ def compile(file,
         exit()
 
     ''' Printing the Abstract Syntax Tree '''
-    if only_ast:
+    if print_ast:
         print(ast)
         exit()
 
+    ''' Semantic analysis '''
     try:
         ast.sem(SymbolTable())
 
@@ -56,15 +57,16 @@ def compile(file,
         exit()
 
     ''' LLVM IR Code Generation '''
-    # llvm_ir = str(ast.codegen(opt_level=0))
+    llvm_ir = str(ast.codegen(opt_level=optimization))
 
     if llvm_to_stdout:
         print(llvm_ir)
+        exit()
     else:
         llvm_output = 'a.ll' if testing else f'{file_prefix}.ll'
 
         with open(llvm_output, 'w') as f:
-            print(ast.codegen(), file=f)
+            print(llvm_ir, file=f)
 
     ''' Compiling to Assembly '''
     as_output = 'a.s' if testing else f'{file_prefix}.s'
@@ -74,14 +76,26 @@ def compile(file,
     if show_commands: print(cmd_llc)
 
     if as_to_stdout:
-        with open(as_output, r) as f:
+        with open(as_output, 'r') as f:
             print(f.read())
+
+        os.remove(as_output)
+        os.remove(llvm_output)
+        exit()
+
 
     ''' Making the final executable '''
     executable = 'a.out' if testing else f'{file_prefix}.out'
     cmd_gcc = f'gcc {as_output} -L . -Wl,-rpath={os.getcwd()} -lbuiltins -o {executable}'
     os.system(cmd_gcc)
     if show_commands: print(cmd_gcc)
+
+    ''' Clean files if running tests '''
+    if testing:
+        os.remove(as_output)
+        os.remove(llvm_output)
+
+    return
 
 
 if __name__ == '__main__':
@@ -91,7 +105,9 @@ if __name__ == '__main__':
     argparser.add_argument('-i', action='store_true')
     argparser.add_argument('-O2', action='store_true')
     argparser.add_argument('-O3', action='store_true')
-    argparser.add_argument('-commands', action='store_true')
+    argparser.add_argument('--commands', action='store_true')
+    argparser.add_argument('--ast', action='store_true')
+
 
     args = argparser.parse_args()
 
@@ -101,9 +117,20 @@ if __name__ == '__main__':
     if args.O3:
         optimization = 3
 
-    if args.f or args.i:
+    if args.f or args.i or args.ast:
         file = None
     else:
         file = args.filename
 
-    compile(file, show_commands=args.commands, optimization=optimization)
+    if args.i and args.f:
+        print('Please specify at most one of the arguments -f and -i')
+        exit()
+
+
+    compile(
+        file,
+        show_commands=args.commands,
+        llvm_to_stdout=args.i,
+        as_to_stdout=args.f,
+        print_ast=args.ast,
+        optimization=optimization)
